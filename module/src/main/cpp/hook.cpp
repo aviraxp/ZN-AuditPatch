ca5cb597af116ec333cbb0df53befcdba7e39022
Wang Han <416810799@qq.com>
2025-08-09 13:15:51 +0800

    Ensure tcontext is not in quotes

--- module/src/main/cpp/hook.cpp
+++ module/src/main/cpp/hook.cpp
@@ -13,36 +13,46 @@ void *handle;
 
 static int (*old_vasprintf)(char **strp, const char *fmt, va_list ap) = nullptr;
 
+static bool is_in_quotes(const char *str, const char *pos) {
+    bool in_quote = false;
+    for (const char *p = str; p < pos; p++) {
+        if (*p == '"') {
+            in_quote = !in_quote;
+        }
+    }
+    return in_quote;
+}
+
 static int my_vasprintf(char **strp, const char *fmt, va_list ap) {
     // https://cs.android.com/android/platform/superproject/main/+/main:system/logging/logd/LogAudit.cpp;l=210
     auto result = old_vasprintf(strp, fmt, ap);
 
     if (result > 0 && *strp) {
-        const char *target_context = "u:r:kernel:s0";
+        const char *target_context = "tcontext=u:r:kernel:s0";
 
         std::vector<const char *> source_contexts = {
-                "u:r:su:s0",
-                "u:r:magisk:s0"
+                "tcontext=u:r:su:s0",
+                "tcontext=u:r:magisk:s0"
         };
 
         size_t target_len = strlen(target_context);
 
         for (const char *source: source_contexts) {
             char *pos = strstr(*strp, source);
-            if (pos) {
+
+            if (pos && !is_in_quotes(*strp, pos)) {
                 size_t source_len = strlen(source);
-
                 size_t extra_space = (target_len > source_len) ? (target_len - source_len) : 0;
-                char *new_str = static_cast<char *>(malloc(result + 2 * extra_space + 1)); // reserve double space in case
-                strcpy(new_str, *strp);
 
-                pos = strstr(new_str, source);
-                if (pos) {
-                    if (source_len != target_len) {
-                        memmove(pos + target_len, pos + source_len, strlen(pos + source_len) + 1);
-                    }
-                    memcpy(pos, target_context, target_len);
+                // Reverse double space in case
+                char *new_str = static_cast<char *>(malloc(result + 2 * extra_space + 1));
+                strcpy(new_str, *strp);
+                pos = new_str + (pos - *strp);
+
+                if (source_len != target_len) {
+                    memmove(pos + target_len, pos + source_len, strlen(pos + source_len) + 1);
                 }
+                memcpy(pos, target_context, target_len);
 
                 free(*strp);
                 *strp = new_str;
